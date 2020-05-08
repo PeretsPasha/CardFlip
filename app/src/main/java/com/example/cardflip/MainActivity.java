16d19763f395b11c2f8305947f339c0847b0d77f
Iron <perts.pasha.pro@gmail.com>
2020-05-08 13:02:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cardflip/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.cardflip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wajahatkarim3.easyflipview.EasyFlipView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
